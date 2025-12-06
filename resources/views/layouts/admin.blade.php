4797719038fcf2583a7d5b8efa5fabd2bff34ecf
ferlita <ferlitamarselia2@gmail.com>
2025-12-06 14:58:27 +0700

    perbarui tittle admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buket-Store Admin</title>
+    <title>Buket Admin</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
@@ -117,7 +117,7 @@
         <!-- LOGO + TITLE -->
         <div class="d-flex align-items-center px-3 mb-4">
             <img src="{{ asset('images/logo-buket-new.png') }}" alt="Logo" class="sidebar-logo">
-            <span class="store-title">Buket Store</span>
+            <span class="store-title">Buket</span>
         </div>
 
         <!-- MENU -->
