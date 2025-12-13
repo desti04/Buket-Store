5cd5c8e4570b744f9104020901108eca6e1c8ab4
Faruq <kembarplayer41@gmail.com>
2025-12-13 15:35:21 +0700

    perubahan sedikit

--- database/migrations/2025_12_13_010924_add_otp_columns_to_users_table.php
+++ database/migrations/2025_12_13_010924_add_otp_columns_to_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('otp_code', 10)->nullable()->after('email_verified_at');
+            $table->string('otp_code', 64)->nullable()->after('email_verified_at');
             $table->timestamp('otp_expires_at')->nullable()->after('otp_code');
         });
     }
